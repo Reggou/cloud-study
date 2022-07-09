5495d9da9acfffae7dec690eb62a2557c5f347d8
wzm <2780828962@qq.com>
2022-07-09 23:28:49 +0800

    restTemplate.getForEntity

--- cloud2020/cloud-consumer-order80/src/main/java/com/wang/controller/OrderController.java
+++ cloud2020/cloud-consumer-order80/src/main/java/com/wang/controller/OrderController.java
@@ -4,12 +4,15 @@ import com.wang.entities.CommonResult;
 import com.wang.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import javax.swing.text.html.parser.Entity;
 
 /**
  * @author wzm
@@ -43,5 +46,17 @@ public class OrderController {
         return restTemplate.getForObject(PAYMENT_SRV+"/payment/get/"+id
                 ,CommonResult.class
         );
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult getPayment2(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_SRV+"/payment/get/"+id
+                ,CommonResult.class
+        );
+        if (entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else {
+            return new CommonResult(50001,"操作失败");
+        }
     }
 }
