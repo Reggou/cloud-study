33bef4c889f676526b6123b1330b029dc824bede
wzm <2780828962@qq.com>
2022-07-27 00:32:22 +0800

    Gateway网关搭建

---
+++ cloud2020/cloud-gateway-gateway9527/src/main/java/com/wang/GateWayMain9527.java
@@ -0,0 +1,19 @@
+package com.wang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wzzmm
+ * @since 2022/7/26 22:07
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
