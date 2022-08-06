3bd8345148f945d27b23a534360b5173f374bb74
wzm <2780828962@qq.com>
2022-08-06 10:37:40 +0800

    客户端配置与测试

---
+++ cloud2020/cloud-config-client-3355/src/main/java/com/wang/ConfigClientMain3355.java
@@ -0,0 +1,17 @@
+package com.wang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wzzmm
+ * @since 2022/8/3 22:39
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+}
---
+++ cloud2020/cloud-config-client-3355/src/main/java/com/wang/controller/ConfigClientController.java
@@ -0,0 +1,21 @@
+package com.wang.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wzzmm
+ * @since 2022/8/3 22:41
+ */
+
+@RestController
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
