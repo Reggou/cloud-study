7e3e3bc8a0492a2c5770e4623c7bfbee4eb344ca
wzm <2780828962@qq.com>
2022-08-06 11:33:46 +0800

    config动态刷新之手动版

--- cloud2020/cloud-config-client-3355/src/main/java/com/wang/controller/ConfigClientController.java
+++ cloud2020/cloud-config-client-3355/src/main/java/com/wang/controller/ConfigClientController.java
@@ -1,15 +1,19 @@
 package com.wang.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * cmd下输入就可实现手动动态刷新  curl -X POST "http://localhost:3355/actuator/refresh"
+ *
  * @author wzzmm
  * @since 2022/8/3 22:41
  */
 
 @RestController
+@RefreshScope //动态刷新
 public class ConfigClientController {
     @Value("${config.info}")
     private String configInfo;
