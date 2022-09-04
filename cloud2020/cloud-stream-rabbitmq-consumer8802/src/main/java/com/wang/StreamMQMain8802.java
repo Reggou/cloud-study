0badb532297253664048525eb86c4da6b93518f7
reggou <2780828962@qq.com>
2022-09-04 17:29:30 +0800

    Stream消息驱动之消费者

---
+++ cloud2020/cloud-stream-rabbitmq-consumer8802/src/main/java/com/wang/StreamMQMain8802.java
@@ -0,0 +1,17 @@
+package com.wang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wzzmm
+ * @since 2022/9/4 17:08
+ */
+@SpringBootApplication
+public class StreamMQMain8802
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StreamMQMain8802.class,args);
+    }
+}
