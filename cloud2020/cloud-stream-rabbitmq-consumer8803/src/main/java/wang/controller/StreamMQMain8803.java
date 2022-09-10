d3798d0219432f9fdcde37b8517825ecd76b6a8b
reggou <2780828962@qq.com>
2022-09-10 16:11:32 +0800

    Stream之group解决消息重复消费和进行消息持久化

---
+++ cloud2020/cloud-stream-rabbitmq-consumer8803/src/main/java/wang/controller/StreamMQMain8803.java
@@ -0,0 +1,17 @@
+package wang.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wzzmm
+ * @since 2022/9/10 15:27
+ */
+@SpringBootApplication
+public class StreamMQMain8803
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
