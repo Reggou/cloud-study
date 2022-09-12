5e1fd6d20277e59c2a15c9bb1dc7df3c683c5bd7
reggou <2780828962@qq.com>
2022-09-12 16:43:14 +0800

    Nacos之服务提供者注册

---
+++ cloud2020/cloudalibaba-provider-payment9001/src/main/java/com/wang/PaymentMain9001.java
@@ -0,0 +1,17 @@
+package com.wang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wzzmm
+ * @since 2022/9/12 16:12
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
---
+++ cloud2020/cloudalibaba-provider-payment9002/src/main/java/wang/PaymentMain9002.java
@@ -0,0 +1,17 @@
+package wang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wzzmm
+ * @since 2022/9/12 16:12
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class, args);
+    }
+}
