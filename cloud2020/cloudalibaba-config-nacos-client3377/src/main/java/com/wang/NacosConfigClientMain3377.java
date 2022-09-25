88041fcbbde39729bd10870b88d7076b98d30823
reggou <2780828962@qq.com>
2022-09-25 12:51:19 +0800

    Nacos之服务配置中心DataID,Group,NameSpace

---
+++ cloud2020/cloudalibaba-config-nacos-client3377/src/main/java/com/wang/NacosConfigClientMain3377.java
@@ -0,0 +1,17 @@
+package com.wang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wzzmm
+ * @since 2022/9/24 17:20
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
