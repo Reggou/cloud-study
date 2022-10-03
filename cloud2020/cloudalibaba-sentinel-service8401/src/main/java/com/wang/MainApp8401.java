1787c2fef59380934706def14994b50095828369
reggou <2780828962@qq.com>
2022-10-03 20:24:55 +0800

    Nacos之sentinel相关配置

---
+++ cloud2020/cloudalibaba-sentinel-service8401/src/main/java/com/wang/MainApp8401.java
@@ -0,0 +1,17 @@
+package com.wang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wzzmm
+ * @since 2022/9/26 22:27
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MainApp8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class, args);
+    }
+}
---
+++ cloud2020/cloudalibaba-sentinel-service8401/src/main/java/com/wang/controller/FlowLimitController.java
@@ -0,0 +1,28 @@
+package com.wang.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author wzzmm
+ * @since 2022/9/26 22:30
+ */
+@RestController
+@Slf4j
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA() {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        return "------testB";
+    }
+}
+
+
